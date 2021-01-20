f82a409dc30090b34e862a0178b831d76ec189df
Yuri Matos de Lima <y.m.lima19@gmail.com>
2021-01-20 12:45:29 +0000

    Password Reset

--- fakenomore/settings.py
+++ fakenomore/settings.py
@@ -139,3 +139,10 @@ AUTH_USER_MODEL = 'users.User'
 
 # Django Crispy Forms
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST ='smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'y.m.lima19@gmail.com'
+EMAIL_HOST_PASSWORD = 'kdtgduffsapwuypd'
--- fakenomore/urls.py
+++ fakenomore/urls.py
@@ -29,6 +29,19 @@ urlpatterns = [
     path('register/', user_views.register, name='register'),#from users import views as user_views
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),#from django.contrib.auth import views as auth_views
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),#from django.contrib.auth import views as auth_views  - 'users/logout.html'
+    #Password Reset
+    path('password-reset/', 
+        auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'),
+         name='password_reset'),
+    path('password-reset/done/', 
+        auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', 
+        auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('password-reset-complete/', 
+        auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+         name='password_reset_complete'),
     #Profiles
     path('profile/', user_views.profile, name='profile'),
     
