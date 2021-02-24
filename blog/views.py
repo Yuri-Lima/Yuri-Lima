f242d3addbca4ecfff5c45178a80cd9ec623efbc
Yuri Matos de Lima <y.m.lima19@gmail.com>
2021-02-24 22:30:28 +0000

    bug3

--- blog/views.py
+++ blog/views.py
@@ -16,7 +16,9 @@ from django.urls import reverse_lazy
 from .forms import PostFormHelper, PainelForm, PostForm
 from django.http import HttpResponseRedirect
 from django.forms.models import inlineformset_factory
-from .covid import covid19
+# from .covid import covid19
+import requests
+from decouple import config
 
 formSet = CombinedFormSet = inlineformset_factory(
             Painel,
@@ -25,6 +27,29 @@ formSet = CombinedFormSet = inlineformset_factory(
             extra=1,
             can_delete=False,
 )
+
+def covid19():
+    country = getcountry()
+    # country = 'Brazil'
+    url = "https://covid-19-data.p.rapidapi.com/country"
+    querystring = {"name":str(country)}
+    headers = {
+    'x-rapidapi-key': config('Rapid_API_Key_Covid'), 
+    'x-rapidapi-host': "covid-19-data.p.rapidapi.com"
+    }
+    responsecovid = requests.request("GET", url, headers=headers, params=querystring).json()
+    return responsecovid
+
+def getcountry():
+    url = "https://find-any-ip-address-or-domain-location-world-wide.p.rapidapi.com/iplocation"
+    querystring = {"apikey":config('Ipworld_API_Key')}
+    headers = {
+        'x-rapidapi-key': config('Rapid_API_Key_Country'),
+        'x-rapidapi-host': "find-any-ip-address-or-domain-location-world-wide.p.rapidapi.com"
+        }
+
+    response = requests.request("GET", url, headers=headers, params=querystring).json()
+    return response['country']
 
 class PainelList(ListView):
     model: Painel
