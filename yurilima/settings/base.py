eb5b6645c355b546da9507aced4eabc50b020f89
Yuri Matos de Lima <y.m.lima19@gmail.com>
2021-03-19 18:04:55 +0000

    Fix_Bug_AWS_Public_Acsses_3

--- yurilima/settings/base.py
+++ yurilima/settings/base.py
@@ -191,8 +191,11 @@ SERVER_EMAIL = "y.m.lima19@gmail.com"  # ditto (default from-email for Django er
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
+# AWS_DEFAULT_ACL = None
+AWS_DEFAULT_ACL = 'public-read'
 AWS_QUERYSTRING_AUTH = False
 # AWS_S3_BUCKET_AUTH = False
 # AWS_S3_GZIP = True
